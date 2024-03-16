deda821f7622f1c26876ca3171554d4f40b43ae4
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-03-16 19:35:24 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-clear
-sitedwn=github.com/PhoenixxZ2023/paineldtunnel
-IP=$(wget -qO- ipv4.icanhazip.com)
+
 [[ "$(whoami)" != "root" ]] && {
 echo
 echo "Instale Com Usuário Root!"
 echo
 rm install.sh
-exit 1
+exit 0
 }
 
 ubuntuV=$(lsb_release -r | awk '{print $2}' | cut -d. -f1)
 
 [[ $(($ubuntuV < 20)) = 1 ]] && {
 clear
-echo "A Versão Do Ubuntu Tem Que Ser No Mínimo 20. A Sua É $ubuntuV"
+echo "A Versão Do Ubuntu Tem Que Ser No Mínimo 20, A Sua É $ubuntuV"
 echo
 rm /root/install.sh
-exit 1
+exit 0
 }
 [[ -e /root/paineldtunnel/src/index.ts ]] && {
   clear
-  echo "O Painel já está instalado. Deseja removê-lo? (s/n)"
+  echo "O Painel já está instalado, deseja remover? (s/n)"
   read remo
   [[ $remo = @(s|S) ]] && {
   cd /root/paineldtunnel
@@ -39,47 +37,38 @@ exit 1
   exit 0
 }
 clear
-echo "QUAL PORTA DESEJA USAR?: "
-read port
+echo "Qual Porta Deseja Ativar?"
+read porta
 echo
-echo "Instalando Painel Dtunnel Mod..."
+echo "Intalando Painel Dtmod..."
 echo
 sleep 3
 #========================
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install wget -y
-sudo apt-get install curl -y
-sudo apt-get install zip -y
-sudo apt-get install cron -y
-sudo apt-get install unzip -y > /dev/null 2>&1
-sudo apt-get install npm -y > /dev/null 2>&1
-sudo apt-get install screen -y
-sudo apt-get install git -y
+apt update -y
+apt-get update -y
+apt install wget -y
+apt install curl -y
+apt install zip -y
+apt install cron -y
+apt install unzip -y
+apt install screen -y
+apt install git -y
 curl -fsSL https://deb.nodesource.com/setup_20.x | bash
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install -y nodejs -y > /dev/null 2>&1
+apt-get install -y nodejs -y
 #=========================
-[[ ! -d /etc/paineldtunnel ]] && mkdir /etc/paineldtunnel
-cd /etc/paineldtunnel || exit
-wget $sitedwn/paineldtunnel.zip > /dev/null 2>&1
-unzip -o paineldtunnel.zip > /dev/null 2>&1
-rm paineldtunnel.zip > /dev/null 2>&1
-cd || exit
-chmod 777 -R /etc/paineldtunnel > /dev/null 2>&1
+git clone https://github.com/PhoenixxZ2023/paineldtunnel.git
 cd /root/paineldtunnel
 chmod 777 pon poff menudt backmod
 mv pon poff menudt backmod /bin
-echo "PORT=$port" > .env
+echo "PORT=$porta" > .env
 echo "NODE_ENV=\"production\"" >> .env
 echo "DATABASE_URL=\"file:./database.db\"" >> .env
 token1=$(node -e "console.log(require('crypto').randomBytes(256).toString('base64'));")
 token2=$(node -e "console.log(require('crypto').randomBytes(256).toString('base64'));")
 token3=$(node -e "console.log(require('crypto').randomBytes(256).toString('base64'));")
-echo "CSRF_SECRET=\"$token1\"" >> .env > /dev/null 2>&1
-echo "JWT_SECRET_KEY=\"$token2\"" >> .env > /dev/null 2>&1
-echo "JWT_SECRET_REFRESH=\"$token3\"" >> .env > /dev/null 2>&1
+echo "CSRF_SECRET=\"$token1\"" >> .env
+echo "JWT_SECRET_KEY=\"$token2\"" >> .env
+echo "JWT_SECRET_REFRESH=\"$token3\"" >> .env
 echo "ENCRYPT_FILES=\"7223fd56-e21d-4191-8867-f3c67601122a\"" >> .env
 npm install
 npx prisma generate
@@ -88,7 +77,7 @@ npx prisma migrate deploy
 clear
 echo
 echo
-echo "PAINEL DTUNNELMOD INSTALADO COM SUCESSO!"
+echo "PAINEL DTUNNEL MOD INSTALADO COM SUCESSO!"
 echo "Os Arquivos Ficam Na Pasta /root/paineldtunnel"
 echo
 echo "Comando para ATIVAR: pon"
@@ -98,12 +87,3 @@ echo "Digite menudt Para ver o menu"
 echo
 rm /root/install.sh
 pon
-echo
-echo -e "\033[1;36m SEU PAINEL:\033[1;37m http://$IP\033[0m"
-echo
-echo -ne "\n\033[1;31mENTER \033[1;33mpara retornar ao \033[1;32mao prompt! \033[0m"; read
-cat /dev/null > ~/.bash_history && history -c
-rm -rf wget-log* > /dev/null 2>&1
-rm install* > /dev/null 2>&1
-clear
-fi
