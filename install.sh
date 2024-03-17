8bf2f6d7f0b4d077e044c993bb4face8fc7b96eb
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-03-16 23:47:55 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -95,10 +95,5 @@ echo -ne "\n\033[1;31mENTER \033[1;33mPara Retornar \033[1;32mAo Prompt! \033[0m
 cat /dev/null > ~/.bash_history && history -c
 rm -rf wget-log* > /dev/null 2>&1
 rm install* > /dev/null 2>&1
-sllep 3
-esac
-done
-}
+sleep 3
 menudt
-#fim
-
