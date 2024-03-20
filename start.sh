8a2d473c45a6e32312aabe9fac1b9ad2a01acf18
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-03-20 20:01:25 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-#!bin/bash
 GREEN='\033[0;32m'
+RESET='\033[0m'
+
 while : 
 do
-echo "iniciando no modo anti queda aguarde..."
+    echo -e "${GREEN}Iniciando no modo anti queda, aguarde...${RESET}"
     npm run dev
     sleep 1
-
 done
