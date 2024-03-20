18280766d9a38c928ecf4240e3684c37f1241322
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-03-20 11:09:46 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,14 +55,9 @@ npm install pm2 -g > /dev/null 2>&1
 sudo apt-get install cron -y
 sudo apt-get install screen -y
 sudo apt-get install git -y
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=21
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install nodejs -y > /dev/null 2>&1
+apt install git -y
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash
+sudo apt-get install -y nodejs -y
 #=========================
 git clone https://github.com/PhoenixxZ2023/paineldtunnel.git
 cd /root/paineldtunnel
