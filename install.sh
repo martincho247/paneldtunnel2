b8e0e9c0cc4fb9b0264ff99b0693680c5cdb6432
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-03-23 00:33:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,6 +54,8 @@ sudo apt-get install npm /dev/null
 npm install pm2 -g /dev/null
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash && export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion" && nvm install 20
 cd paineldtunnel && wget https://raw.githubusercontent.com/PhoenixxZ2023/paineldtunnel/main/v18.js
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=21
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
