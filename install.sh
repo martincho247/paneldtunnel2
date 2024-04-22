ef4cb56ba9d45c16c19d7bdb6e6692e1fc5136ee
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-04-21 21:13:33 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -52,17 +52,10 @@ sudo apt-get install curl -y
 sudo apt-get install zip -y
 sudo apt-get install npm -y /dev/null
 npm install pm2 -g /dev/null
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash && export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion" && nvm install 20
-cd paineldtunnel && wget https://raw.githubusercontent.com/PhoenixxZ2023/paineldtunnel/main/v18.js
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=21
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get install cron -y
+sudo apt-get install unzip -y
 sudo apt-get install screen -y
 sudo apt-get install git -y
-apt install git -y
 curl -s -L https://raw.githubusercontent.com/PhoenixxZ2023/paineldtunnel/main/setup_20.x | bash
 sudo apt-get install nodejs -y
 #=========================
@@ -73,9 +66,9 @@ mv pon poff menudt backmod /bin
 echo "PORT=$porta" > .env
 echo "NODE_ENV=\"production\"" >> .env
 echo "DATABASE_URL=\"file:./database.db\"" >> .env
-token1=$(node -e "console.log(require('crypto').randomBytes(256).toString('base64'));")
-token2=$(node -e "console.log(require('crypto').randomBytes(256).toString('base64'));")
-token3=$(node -e "console.log(require('crypto').randomBytes(256).toString('base64'));")
+token1=$(node -e "console.log(require('crypto').randomBytes(100).toString('base64'));")
+token2=$(node -e "console.log(require('crypto').randomBytes(100).toString('base64'));")
+token3=$(node -e "console.log(require('crypto').randomBytes(100).toString('base64'));")
 echo "CSRF_SECRET=\"$token1\"" >> .env
 echo "JWT_SECRET_KEY=\"$token2\"" >> .env
 echo "JWT_SECRET_REFRESH=\"$token3\"" >> .env
